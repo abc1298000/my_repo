6ee7c4e511bb9b7ef998df1dda75679e00a0e6ef
abc1298000 <91632907+abc1298000@users.noreply.github.com>
2021-10-06 10:31:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 import anvil.server
-import numpy as np
+import time
+from urllib.request import urlopen
 import pandas as pd
 import pickle
-from urllib.request import urlopen
+import warnings
 
-link = urlopen(r"https://github.com/abc1298000/my_repo/raw/eed8bcf0268ee7d2f6c9de87efe317bf8c5eccb6/pipeline.pkl")
-data_pipeline = pickle.load(link)
-
-cols = ['age_logarithmic', 'education_rank', 'hours_per_week', 'marital_status',\
-    'sex', 'gain/loss', 'Local_gov', 'Private', 'Self_emp_inc',\
-    'Self_emp_not_in_inc', 'State_gov', 'others', 'Asian_Pac_Islander',\
-    'Black', 'Other', 'White', 'Armed_Forces', 'Craft_repair',\
-    'Exec_managerial', 'Farming_fishing', 'Handlers_cleaners',\
-    'Machine_op_inspct', 'Other_service', 'Priv_house_serv',\
-    'Prof_specialty', 'Protective_serv', 'Sales', 'Tech_support',\
-    'Transport_moving', 'El Salvador', 'Germany', 'India', 'Mexico',\
-    'Philippines', 'Puerto Rico', 'United States', 'others']
+warnings.filterwarnings('ignore')
 
 
-dct = {'10th': 6.0,'11th': 7.0,'12th': 8.0,'1st-4th': 2.0,'5th-6th': 3.0,'7th-8th': 4.0,\
-'9th': 5.0,'Assoc-acdm': 12.0, 'Assoc-voc': 11.0,\
-'Bachelors': 13.0, 'Doctorate': 16.0,'HS-grad': 9.0, 'Masters': 14.0,'Preschool': 1.0,\
-    'Prof-school': 15.0,'Some-college': 10.0}
-
-
-anvil.server.connect("UWAXNIPZNKJ7SFTIJ7DRTWXP-XEZ5Q6KTQRZIXOLS")
+anvil.server.connect("server_BT6EO7O6OJWWUG4GZ3ZLJAMY-RGKODPUNK74BJTQ6")
 
 @anvil.server.callable
-def predict(age, education, hours_per_week, marital_status, sex, gain_or_loss, workclass, race, occupation, country):
-    x = np.zeros(37)
+def pred(age, education, hours_per_week, marital_status, sex, gain_or_loss, workclass, race, occupation, country):
+
+    cols = ['age_logarithmic', 'education_rank', 'hours_per_week', 'marital_status',\
+        'sex', 'gain/loss', 'Local_gov', 'Private', 'Self_emp_inc',\
+        'Self_emp_not_in_inc', 'State_gov', 'others', 'Asian_Pac_Islander',\
+        'Black', 'Other', 'White', 'Armed_Forces', 'Craft_repair',\
+        'Exec_managerial', 'Farming_fishing', 'Handlers_cleaners',\
+        'Machine_op_inspct', 'Other_service', 'Priv_house_serv',\
+        'Prof_specialty', 'Protective_serv', 'Sales', 'Tech_support',\
+        'Transport_moving', 'El Salvador', 'Germany', 'India', 'Mexico',\
+        'Philippines', 'Puerto Rico', 'United States', 'others']
+    
+    
+    dct = {'10th': 6.0,'11th': 7.0,'12th': 8.0,'1st-4th': 2.0,'5th-6th': 3.0,'7th-8th': 4.0,\
+           '9th': 5.0,'Assoc-acdm': 12.0, 'Assoc-voc': 11.0,'Bachelors': 13.0, 'Doctorate': 16.0,'HS-grad': 9.0, 'Masters': 14.0,'Preschool': 1.0,\
+           'Prof-school': 15.0,'Some-college': 10.0}
+ 
+    path = urlopen(r'https://github.com/abc1298000/my_repo/raw/main/pipeline.pkl')
+    data_pipeline = pickle.load(path)
+    
+    x = [0 for x in range(1, 38)]
     x[0] = age
     x[1] = dct[education]
     x[2] = hours_per_week
@@ -59,6 +62,10 @@ def predict(age, education, hours_per_week, marital_status, sex, gain_or_loss, w
     test_row = pd.DataFrame(x).transpose()
     test_row.columns = cols
     result = data_pipeline.predict(test_row)
+    
+    time.sleep(3)
 
     return result[0]
+
+
 anvil.server.wait_forever()
